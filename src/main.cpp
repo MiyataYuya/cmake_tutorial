5aa1774dd06b3f54462721b86ce5ce0e91061a8e
MiyataYuya <katorisenkouhanabi@gmail.com>
2022-10-10 21:07:24 +0900

    versioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "adder.h"
+#include <GLFW/glfw3.h>
 
 int main()
 {
@@ -8,4 +9,30 @@ int main()
     std::cout << "Hello, world!!\n";
 
     std::cout << add(a, b) << std::endl;
+
+
+
+
+    GLFWwindow *window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    // Terminate GLFW
+    glfwTerminate();
 }
